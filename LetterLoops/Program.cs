b3f1cb28ea3f60c4288842391a07da54e36ebf4c
John Maple <jmaple@bellsouth.net>
2021-07-17 11:10:56 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string line = Console.ReadLine();
+            line = line.ToLower().Trim();
+            string outputStr = String.Empty;
+            Console.WriteLine("You entered " + line);
+            for (var i = 0; i < line.Length; i++)
+            {
+                outputStr += char.ToUpper(line[i]);
+                for (var j = 0; j < i; j++)
+                {
+                    outputStr += line[i];
+                }
+                if (i < line.Length - 1)
+                {
+                    outputStr += '-';
+                }
+            }
+            Console.WriteLine(outputStr);
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
